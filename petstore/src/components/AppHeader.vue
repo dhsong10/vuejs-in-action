f6d7d339f46829b469256ad94e2e04b383379258
dhsong10 <david.dh.song@gmail.com>
2021-02-12 23:34:24 +0900

    [ADD] header with bootstrap styling

---
+++ petstore/src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-navbar class="p-3" type="dark" variant="dark">
+    <b-navbar-brand class="ml-3" href="/">{{ sitename }}</b-navbar-brand>
+
+    <b-narvar-nav class="ml-auto mr-3">
+      <b-button variant="primary">
+        <i class="fa fa-cart-arrow-down mr-3" /> 체크아웃
+      </b-button>
+    </b-narvar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sitename: 'Vue.js 애완동물샵',
+    };
+  },
+};
+</script>
--- petstore/src/views/Home.vue
+++ petstore/src/views/Home.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>Home</div>
+  <div>
+    <AppHeader />
+  </div>
 </template>
 
 <script>
+import AppHeader from '@/components/AppHeader.vue';
 export default {
   name: 'Home',
+  components: {
+    AppHeader,
+  },
 };
 </script>
